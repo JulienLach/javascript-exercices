683228338d028791924a7de703167852a73af18c
JulienLach <leadedchamps@gmail.com>
2023-12-13 12:13:08 +0100

    ok

--- exercices_varaibles/script.js
+++ exercices_varaibles/script.js
@@ -12,12 +12,12 @@
 
 // EXERCICE 3
 
-// let prenom = "Jean";
-// let nom = "Dupont";
-// let age = 30;
-// let ville = "Paris";
+//  let prenom = "Jean";
+//  let nom = "Dupont";
+//  let age = 30;
+//  let ville = "Paris";
 
-// console.log("Bonjour, je m'appelle " + prenom + " " + nom + ", j'ai " + age + "ans et j'habite " + ville);
+// console.log("Bonjour, je m'appelle " + prenom + " " + nom + ", j'ai " + age + " ans et j'habite " + ville);
 
 
 // EXERCICE 4
@@ -56,16 +56,99 @@
 
 // EXERCICE 7
 
-let nombre1 = window.prompt("Entrez le premier nombre :");
-let nombre2 = window.prompt("Entrez le deuxième nombre :");
+// let nombre1 = parseFloat(window.prompt("Entrez le premier nombre :"));
+// let nombre2 = parseFloat(window.prompt("Entrez le premier nombre :"));
 
-parseFloat(nombre1)
-parseFloat(nombre2)
+// // parseFloat(nombre1)
+// // parseFloat(nombre2)
 
-let somme = nombre1 + nombre2   // problème ici : ajout de 2 chaines de caratères
-let produit = nombre1 * nombre2
-let difference = nombre1 - nombre2
-let division = nombre1 / nombre2
-let modulo = nombre1 % nombre2
+// let somme = nombre1 + nombre2
+// let produit = nombre1 * nombre2
+// let difference = nombre1 - nombre2
+// let division = nombre1 / nombre2
+// let modulo = nombre1 % nombre2
 
-alert("somme = " + somme + "\n" + "produit = " + produit + "\n" + "difference = " + difference + "\n" + "division = " + division + "\n" + "modulo = " + modulo)
+// alert("somme = " + somme + "\n" + "produit = " + produit + "\n" + "difference = " + difference + "\n" + "division = " + division + "\n" + "modulo = " + modulo)
+
+
+// EXERCICE 8
+
+// let ageConfirmé = true;
+
+// while (ageConfirmé) {
+//   let reponse = window.prompt("Entrez votre âge :");
+//   let verif_reponse = window.confirm("Êtes-vous sûr d'être majeur ?");
+
+//   if (verif_reponse) {
+//     if (reponse >= 18) {
+//       alert("Vous êtes majeur");
+//     } else {
+//       alert("Vous êtes mineur");
+//     }
+//     ageConfirmé = false; // on sort de la boucle si l'utilisateur à confirmé son âge
+//   } else {
+//     alert("Veuillez confirmer votre âge !");
+//   }
+// }
+
+
+// EXERCICE 9
+
+// a = "20"
+// b = 20
+
+// egaliteSimple = a == b
+// egaliteStricte = a === b
+
+// console.log(egaliteSimple)
+// console.log(egaliteStricte)
+
+// alert("Pour légalité simple, la chaine de caratères '20' est égale au nombre 20 car l'opérateur == transforme la chaine de caratères en chiffre" )
+// alert("Pour l'égalité stricte compare le type et la valeur, ici un tableau et un nombre donc ça retourne False. les === ne transforme pas le tableau en nombre")
+
+
+// EXERCICE 10
+
+// entreprises = ["Google", "Facebook", "Apple", "Microsoft", "IBM"]
+
+// entreprises.forEach(entreprise => {
+//     console.log(entreprise)
+// });
+
+
+// EXERCICE 11
+
+// fruits = ["Raisin", "Poire", "Pomme", "Mangue", "Citron", "Banane"]
+
+// console.log(fruits[0])
+
+
+
+// EXERCICE 12
+
+let ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+// Fonction de tri du tableau par ordre croissant
+ages.sort(function(a, b) {
+    return a - b;
+  });
+  console.log("Liste des âges triée :", ages);
+
+
+function Mediane(ages) {
+  // refaire le tri des ages dans l'ordre croissant pour faire la mediane
+  ages.sort(function(a, b) {
+    return a - b;
+  });
+
+  function calculMediane(ages) {
+    valeurMediane = ((ages[0] + ages[1] + ages[2] + ages[3] + ages[4]) + (ages[5] + ages[6] + ages[7] + ages[8] + ages[9]) / 2) 
+    return valeurMediane
+}
+
+return calculMediane(ages);
+}
+
+let resultatMediane = Mediane(ages);
+
+console.log("Médiane :", resultatMediane);
