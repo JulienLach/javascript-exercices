0890815a08e83afa1993ace68708319eca2ccffa
JulienLach <leadedchamps@gmail.com>
2023-12-14 10:31:03 +0100

    ok

---
+++ exercices_boucles/script.js
@@ -0,0 +1,38 @@
+// EXERCICE 1   // revoir for in , for of , et forEach, while , et  do ... while
+
+// let nombres = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// nombres.forEach(nombre => {
+//     console.log(nombre)
+// });
+
+// for (let nombre of nombres) {
+//     console.log(nombre)
+// }
+
+// let nombre = 0;
+// while (nombre <= nombres.length) {
+//     console.log(nombre);
+//     nombre++;
+// }
+
+// let nombre = 0;
+// do {
+//     console.log(nombre);
+//     nombre++;
+// } while(nombre <= nombres.length)
+
+
+// EXERCICE 2 // inverser l'affichage de l'exercice 1
+
+let nombres = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+// nombres.forEach(nombre => {
+//     console.log(nombre)
+// });
+
+let nombre = 10;
+while (nombre >= 1) {
+    console.log(nombre);
+    nombre--;
+}
