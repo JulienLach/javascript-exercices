5249c2689ed9e326c009e039771fa0d98ff1e4c8
JulienLach <leadedchamps@gmail.com>
2023-12-15 16:53:43 +0100

    ok

--- exercices_bonus/script.js
+++ exercices_bonus/script.js
@@ -10,10 +10,21 @@
 // EXERCICE 3
 // Créez une fonction qui prend un nombre en argument, incrémente le nombre +1 et renvoie le résultat.
 
-function incrementation(x) {
-    return ++x;
+// function incrementation(x) {
+//     return ++x;
+// }
+// console.log(incrementation(2))
+
+// EXERCICE 4
+/* Écrivez une fonction qui prend la base et la hauteur d’un triangle et retourne sa surface. 
+Notez que la surface d’un triangle est: (base * hauteur) / 2
+*/
+
+function calculSurfaceTriangle(base, hauteur) {
+    surface = (base * hauteur) / 2;
+    return surface
 }
-console.log(incrementation(2))
+console.log(calculSurfaceTriangle(4, 23))
 
 
 
