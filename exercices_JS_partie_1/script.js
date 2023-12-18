e683741285ad5639d6935f18d1b1a9ae06ac1b6b
JulienLach <leadedchamps@gmail.com>
2023-12-18 14:03:48 +0100

    ok

--- exercices_JS_partie_1/script.js
+++ exercices_JS_partie_1/script.js
@@ -26,10 +26,27 @@
 Notez que la surface d’un triangle est: (base * hauteur) / 2
 */
 
-function calculSurfaceTriangle(base, hauteur) {
-    surface = (base * hauteur) / 2;
-    return surface
+// function calculSurfaceTriangle(base, hauteur) {
+//     surface = (base * hauteur) / 2;
+//     return surface
+// }
+// console.log(calculSurfaceTriangle(4, 27))
+
+
+// EXERCICE 5
+/*
+Écrivez un programme JavaScript pour inverser une chaîne de caractères.
+*/
+
+function strReverse(str) {
+    return str.split('').reverse().join('');
+    //1 : couper la string, 2: reverse les lettres coupées, 3: recoller les lettres
 }
-console.log(calculSurfaceTriangle(4, 23))
+console.log(strReverse("Bonjour je m'appel bob"));
+console.log(strReverse("Hello"));
 
 
+// EXERCICE 6
+/*
+Écrivez un programme JavaScript pour trouver le plus grand des trois entiers donnés. 
+*/
