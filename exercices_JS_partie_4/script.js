40859f8635b18db77064f1613bc45d81320b4693
JulienLach <leadedchamps@gmail.com>
2023-12-19 09:21:58 +0100

    ok

--- exercices_JS_partie_4/script.js
+++ exercices_JS_partie_4/script.js
@@ -1,18 +1,19 @@
 // EXERCICE 1
 // Créez une fonction pour concaténer deux tableaux entiers.
 
-function concatener(tableau1, tableau2) {
-    return [tableau1 + "," + tableau2]
-}
+// function concatener(tableau1, tableau2) {
+//     return [tableau1 + "," + tableau2]
+// }
 //corrigé
-function concatener(tab1, tab2) {
-    return [...tab1,...tab2]
-  }
+// function concatener(tab1, tab2) {  // différence spread... et variable tableau1 qui prend tout quand meme
+//     return [...tab1,...tab2]
+//   }
 // Les trois points (…) représentent l’opérateur « Spread » dans ES6 qui 
 // permet à une expression d’être élargi.
-console.log(concatener([1, 2], [3, 4]));
-console.log(concatener([9, 8], [1, 5, 6, 7]));
-console.log(concatener([1, 1, 1], [2, 2, 2]));
+// console.log(concatener([1, 2], [3, 4]));
+// console.log(concatener([9, 8], [1, 5, 6, 7]));
+// console.log(concatener([1, 1, 1], [2, 2, 2]));
+
 
 
 // EXERCICE 2
@@ -20,10 +21,10 @@ console.log(concatener([1, 1, 1], [2, 2, 2]));
 Si l’élément n’existe pas, retournez -1.*/
 
 function trouverIndexValeur(tableau, valeur) {
-	// Écrivez votre code ici
+    for (valeur in tableau) {
+        return valeur
+    }
 }
-
-// Afficher la sortie
-console.log(find([2, 7, 1], 7));
-console.log(find([6, 9, 5], 6));
-console.log(find([6, 7, 8], 9));
+console.log(trouverIndexValeur([2, 7, 1], 7));
+console.log(trouverIndexValeur([6, 9, 5], 6));
+console.log(trouverIndexValeur([6, 7, 8], 9));
