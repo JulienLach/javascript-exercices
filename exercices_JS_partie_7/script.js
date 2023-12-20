177602513a9693963472a34484e171b93ede6bda
JulienLach <leadedchamps@gmail.com>
2023-12-20 16:44:41 +0100

    ok

---
+++ exercices_JS_partie_7/script.js
@@ -0,0 +1,56 @@
+// EXERCICE 1
+// Créez une fonction qui trouve tous les nombres pairs de 1 au nombre donné.
+// function trouverNombresPairs(nombreATester) {
+//     let nombresPairsTableau = [];
+
+//     for (let x = 1; x < nombreATester; x++) {
+//         if (x % 2 == 0) {
+//             nombresPairsTableau.push(x);
+//         }
+//     }
+
+//     return nombresPairsTableau
+// }
+// console.log(trouverNombresPairs(26));
+// console.log(trouverNombresPairs(6));
+// console.log(trouverNombresPairs(4));
+// console.log(trouverNombresPairs(2));
+
+
+
+// EXERCICE 2
+// Écrivez une fonction qui renvoie TRUE si tous les caractères d’une chaîne sont identiques, 
+// sinon renvoie FALSE.
+/*This version uses split('') to convert the string into an array of characters 
+and then uses the every method to check if every character is equal to the first 
+character of the string. If any character is different, it will return false; otherwise, 
+it returns true.
+*/
+// function verifCaracteres(chaine) {
+//     return chaine.split("").every(lettre => lettre === chaine[0])
+// }
+// console.log(verifCaracteres("bbbbbb"));
+// console.log(verifCaracteres("abababab"));
+// console.log(verifCaracteres("111111"));
+
+
+
+// EXERCICE 3
+// Vous avez un tableau de nombre, transformez ce tableau en miroir.
+function miroir(tableau) {
+    return tableau.concat(tableau.slice().reverse())
+}
+console.log(miroir([1, 2, 3]));
+console.log(miroir([9, 8, 7]));
+console.log(miroir([10, 20]));
+
+
+
+// EXERCICE 4 
+// Écrivez une fonction récursive qui trouve la somme des n premiers nombres naturels.
+function somme(nombre) {
+    
+};
+console.log(somme(3));
+console.log(somme(2));
+console.log(somme(10));
