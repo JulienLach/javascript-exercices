4205ca7b085c1414230dd35a12ff2679ba91ef07
JulienLach <leadedchamps@gmail.com>
2024-01-03 11:08:17 +0100

    ok exo films JSON

--- exercice_TMMDB/script.js
+++ exercice_TMMDB/script.js
@@ -4,10 +4,10 @@ const movies = await reponse.json();
 
 for (let i = 0; i < movies.results.length; i++) {
     // création des fiches films
-    const movie = movies.results[i] // premier film dans le JSON
-    const sectionFilms = document.querySelector(".section-films");
+    const movie = movies.results[i] // attraper tous le films avec la boucle for
+    const sectionFilms = document.querySelector(".section-films"); // definir la section qui contient toutes les fiches de films
 
-    // création de la fiche article film
+    // création de la fiche article film qui contient toutes les infos du JSON
     const ficheMovie = document.createElement("article")
 
     const imageMovie = document.createElement("img"); // création balise image
@@ -22,13 +22,12 @@ for (let i = 0; i < movies.results.length; i++) {
     const noteMovie = document.createElement("p");
     noteMovie.innerText = (movie.vote_average + " / 10");
 
-
     // attacher les articles fiches films à la section section-films
     sectionFilms.appendChild(ficheMovie)
 
     // attacher les éléments des films à la fiche film
-    ficheMovie.appendChild(titreMovie);
     ficheMovie.appendChild(imageMovie);
+    ficheMovie.appendChild(titreMovie);
     ficheMovie.appendChild(descriptionMovie);
     ficheMovie.appendChild(noteMovie);
 }
